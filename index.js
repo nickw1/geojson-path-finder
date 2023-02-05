bb8c8c668a578ae0b773b7b750ca677f9d3e6f18
Nick Whitelegg <nickw4426@gmail.com>
2023-02-05 19:32:14 +0000

    v1.6.2: Add vertex keys to edges of calculated route

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ PathFinder.prototype = {
                     ? path.reduce(function buildEdgeData(eds, v, i, vs) {
                         if (i > 0) {
                             eds.push({
-                                reducedEdge: this._graph.compactedEdges[vs[i - 1]][v]
+                                reducedEdge: Object.assign({v1: vs[i-1], v2: v}, this._graph.compactedEdges[vs[i - 1]][v])
                             });
                         }
 
